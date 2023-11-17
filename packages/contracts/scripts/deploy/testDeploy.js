ad9fb13f8a5ec6c13c759c510910a9a9f4407f59
“web3skeptic” <the.mark.fender@gmail.com>
2023-11-17 16:01:50 +0100

    fix: deploy script

--- packages/contracts/scripts/deploy/testDeploy.js
+++ packages/contracts/scripts/deploy/testDeploy.js
@@ -19,7 +19,7 @@ async function initDeploymentSetup() {
   const GasOrderFactory = await ethers.getContractFactory("GasOrder")
 
   const GasOrderContractAddress = await precalculateAddress(admin, 1)
-  const ExecutorContract = await ExecutorFactory.deploy(GasOrderContractAddress, PROJECT_NAME, PROJECT_VERSION, 1, [])
+  const ExecutorContract = await ExecutorFactory.deploy(GasOrderContractAddress, PROJECT_NAME, PROJECT_VERSION)
   await ExecutorContract.deploymentTransaction().wait()
   // @todo add deploy error handling
   console.log(`Executor contract deployed: ${ExecutorContract.target}`)
@@ -29,6 +29,7 @@ async function initDeploymentSetup() {
   console.log(`GasOrder contract deployed: ${GasOrderContract.target}`)
 
   const TokenFactory = await ethers.getContractFactory("MockToken")
+  //@todo replace magic number with testing contracts
   const TokenContract = await TokenFactory.deploy("MockUSD", "MUSD", "1000000000000000000000000") // @todo use ethers function to specify token amount
   await TokenContract.deploymentTransaction().wait()
 
@@ -39,7 +40,7 @@ async function initDeploymentSetup() {
   // @dev send mock tokens to the tester hardhat address
   await TokenContract.transfer("0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65", "100000000000000000000000")
   // Create and accept order
-  await TokenContract.transfer(accounts[10].address, "500000000")
+  await TokenContract.transfer(accounts[10].address, "500000000000000")
   for (let i = 0; i < TOTAL_TEST_ORDERS_AMOUNT; i++) {
     let isAccepted = Math.random() < 0.5
     await createOrder(
