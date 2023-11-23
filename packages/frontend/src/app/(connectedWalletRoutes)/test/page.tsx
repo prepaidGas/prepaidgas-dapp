c44ddc049410c8c0312788f162b13a8e43009a3f
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2023-11-23 16:50:05 +0200

    WIP Testing Page #1

--- packages/frontend/src/app/(connectedWalletRoutes)/test/page.tsx
+++ packages/frontend/src/app/(connectedWalletRoutes)/test/page.tsx
@@ -1,8 +1,9 @@
 "use client"
 // @ts-nocheck
-import { BytesLike, ethers } from "ethers"
+import { BytesLike, SignatureLike, ethers } from "ethers"
 import { signTypedData, readContract } from "@wagmi/core"
 import { TypedDataDomain } from "viem"
+import { PROJECT_NAME, PROJECT_VERSION, CHAIN_ID } from "constants/executor"
 
 import { XMarkIcon } from "@heroicons/react/24/outline"
 import { Card, Title, Subtitle, Text, Grid, Color, Button, TextInput } from "@tremor/react"
@@ -10,42 +11,35 @@ import { useEffect, useState } from "react"
 import JsonFormatter from "react-json-formatter"
 import { z } from "zod"
 import { GasOrderABI } from "helpers/abi"
-
-// interface SigningState {
-//   dataStructure: any
-//   dataTypes: any
-//   parsedData: any
-//   message: any
-// }
+import { useAccount } from "wagmi"
 
 interface VerifySignedState {
   message: any
 }
 
 const schemaSigning = z.object({
-  dataStructure: z.any(),
   dataTypes: z.any(),
-  parsedData: z.any(),
   message: z.string().min(1),
+  parsedTypes: z.any(),
+  parsedMessage: z.any(),
 })
-
-const zodMessageParser = z.string().min(1)
 
 type SigningState = z.infer<typeof schemaSigning>
 
 export default function TestPage() {
   const signingInitialValues = {
-    dataStructure:
-      '[{"name": "from"}, {"name": "nonce"}, {"name": "gasOrder"}, {"name": "onBehalf"}, {"name": "deadline"}, {"name": "to"}, {"name": "gas"}, {"name": "data"}]',
-    dataTypes:
-      '[{"type": "address"}, {"type": "uint256"}, {"type": "uint256"}, {"type": "address"}, {"type": "uint256"}, {"type": "address"}, {"type": "uint256"}, {"type": "bytes"}]',
-    parsedData: undefined,
     message: "",
+    dataTypes:
+      '[{"name":"from","type":"address"},{"name":"nonce","type":"uint256"},{"name":"gasOrder","type":"uint256"},{"name":"onBehalf","type":"address"},{"name":"deadline","type":"uint256"},{"name":"to","type":"address"},{"name":"gas","type":"uint256"},{"name":"data","type":"bytes"}]',
+    parsedData: undefined,
+    parsedMessage: undefined,
   }
 
   const signedInitialValues = {
     message: "",
   }
+
+  const { address, isConnecting, isDisconnected } = useAccount()
 
   const [validationTimer, setValidationTimer] = useState<NodeJS.Timeout | undefined>()
   const [inputValuesForSigning, setInputValuesForSigning] = useState<SigningState>(signingInitialValues)
@@ -55,20 +49,11 @@ export default function TestPage() {
 
   const [isDataParsed, setIsDataParsed] = useState(false)
 
-  const combineDataAndTypesJson = () => {
-    const parsedStructure = JSON.parse(inputValuesForSigning.dataStructure)
+  const parseSigningInputs = () => {
+    const parsedMessage = JSON.parse(inputValuesForSigning.message)
     const parsedTypes = JSON.parse(inputValuesForSigning.dataTypes)
 
-    if (parsedStructure.length !== parsedTypes.length) return
-
-    let combined = []
-
-    for (let index = 0; index < parsedStructure.length; index++) {
-      combined.push({ ...parsedStructure[index], ...parsedTypes[index] })
-    }
-    console.log("Combined ", combined)
-
-    setInputValuesForSigning({ ...inputValuesForSigning, parsedData: combined })
+    setInputValuesForSigning({ ...inputValuesForSigning, parsedTypes, parsedMessage })
     setIsDataParsed(true)
   }
 
@@ -90,24 +75,11 @@ export default function TestPage() {
 
   const signMessage = async () => {
     const domain = {
-      name: "Prepaid Gas",
-      version: "0.0.1",
-      chainId: 31337,
-      verifyingContract: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
-    } as TypedDataDomain
-
-    // The named list of all type definitions
-    // const types = {
-    //   Person: [
-    //     { name: "name", type: "string" },
-    //     { name: "wallet", type: "address" },
-    //   ],
-    //   Mail: [
-    //     { name: "from", type: "Person" },
-    //     { name: "to", type: "Person" },
-    //     { name: "contents", type: "string" },
-    //   ],
-    // }
+      name: PROJECT_NAME,
+      version: PROJECT_VERSION,
+      chainId: CHAIN_ID,
+      verifyingContract: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    }
 
     const types = {
       Message: [
@@ -122,8 +94,10 @@ export default function TestPage() {
       ],
     }
 
+    /*
     const message = {
-      from: "0x376a2a023a105bc2e19ce19ad275b9bbbcb23e1a",
+      //@todo get address automatically
+      from: "0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65",
       nonce: 0,
       gasOrder: 0,
       onBehalf: "0x00222290dd7278aa3ddd389cc1e1d165cc4bafe5",
@@ -132,7 +106,22 @@ export default function TestPage() {
       gas: 0,
       data: "0x",
     }
+    */
 
+    const message = {
+      //@todo get address automatically
+      from: address,
+      nonce: inputValuesForSigning.parsedMessage.nonce,
+      gasOrder: inputValuesForSigning.parsedMessage.gasOrder,
+      onBehalf: inputValuesForSigning.parsedMessage.onBehalf,
+      deadline: inputValuesForSigning.parsedMessage.deadline,
+      to: inputValuesForSigning.parsedMessage.to,
+      gas: inputValuesForSigning.parsedMessage.gas,
+      //todo encode data
+      data: "0x",
+    }
+
+    //todo check typescript
     // @ts-ignore
     const signature = await signTypedData({
       domain,
@@ -161,7 +150,7 @@ export default function TestPage() {
         args: [messageTuple],
       })
       console.log("Data: ", data)
-      const result = ethers.recoverAddress(data as BytesLike, signature)
+      const result = ethers.recoverAddress(data as BytesLike, signature as SignatureLike)
       console.log("RecoverAddres: ", result)
     } catch (e) {
       console.log("ERROR: ", e)
@@ -197,36 +186,39 @@ export default function TestPage() {
 
       <Subtitle className="mt-6">Sign a message</Subtitle>
       <Card className="mt-2">
-        <div className="flex flex-col lg:flex-row gap-6 mt-4">
-          <div className="flex flex-col grow">
-            <Text>Arbitrary message</Text>
-            <TextInput
-              className="mt-2"
-              value={inputValuesForSigning.message}
-              onChange={(e) => setInputValuesForSigning({ ...inputValuesForSigning, message: e.target.value })}
-              placeholder={"Enter a message that you want to sign"}
-              error={!!validationErrors?.message}
-              errorMessage={validationErrors?.message}
-              spellCheck={false}
-            ></TextInput>
-          </div>
-        </div>
         {isDataParsed ? (
-          <div className="flex flex-col lg:flex-row gap-6 mt-4">
-            <div className="flex flex-col grow">
-              <Text>Parsed data</Text>
-              <div className="max-h-[30rem] overflow-auto mt-2 tremor-TextInput-root flex flex-col relative w-full  min-w-[10rem] outline-none rounded-tremor-default shadow-tremor-input dark:shadow-dark-tremor-input bg-tremor-background dark:bg-dark-tremor-background hover:bg-tremor-background-muted dark:hover:bg-dark-tremor-background-muted text-tremor-content dark:text-dark-tremor-content border-tremor-border dark:border-dark-tremor-border border">
-                <JsonFormatter
-                  json={JSON.stringify(inputValuesForSigning.parsedData)}
-                  tabWith={4}
-                  jsonStyle={{
-                    propertyStyle: { color: "rgb(59 130 246)" },
-                    stringStyle: { color: "rgb(16 185 129)" },
-                  }}
-                />
+          <>
+            <div className="flex flex-col lg:flex-row gap-6 mt-4">
+              <div className="flex flex-col grow">
+                <Text>Parsed data</Text>
+                <div className="max-h-[30rem] overflow-auto mt-2 tremor-TextInput-root flex flex-col relative w-full  min-w-[10rem] outline-none rounded-tremor-default shadow-tremor-input dark:shadow-dark-tremor-input bg-tremor-background dark:bg-dark-tremor-background hover:bg-tremor-background-muted dark:hover:bg-dark-tremor-background-muted text-tremor-content dark:text-dark-tremor-content border-tremor-border dark:border-dark-tremor-border border">
+                  <JsonFormatter
+                    json={JSON.stringify(inputValuesForSigning.parsedTypes)}
+                    tabWith={4}
+                    jsonStyle={{
+                      propertyStyle: { color: "rgb(59 130 246)" },
+                      stringStyle: { color: "rgb(16 185 129)" },
+                    }}
+                  />
+                </div>
               </div>
             </div>
-          </div>
+            <div className="flex flex-col lg:flex-row gap-6 mt-4">
+              <div className="flex flex-col grow">
+                <Text>Parsed data</Text>
+                <div className="max-h-[30rem] overflow-auto mt-2 tremor-TextInput-root flex flex-col relative w-full  min-w-[10rem] outline-none rounded-tremor-default shadow-tremor-input dark:shadow-dark-tremor-input bg-tremor-background dark:bg-dark-tremor-background hover:bg-tremor-background-muted dark:hover:bg-dark-tremor-background-muted text-tremor-content dark:text-dark-tremor-content border-tremor-border dark:border-dark-tremor-border border">
+                  <JsonFormatter
+                    json={JSON.stringify(inputValuesForSigning.parsedMessage)}
+                    tabWith={4}
+                    jsonStyle={{
+                      propertyStyle: { color: "rgb(59 130 246)" },
+                      stringStyle: { color: "rgb(16 185 129)" },
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </>
         ) : (
           <>
             <div className="flex flex-col lg:flex-row gap-6 mt-4">
@@ -234,11 +226,11 @@ export default function TestPage() {
                 <Text>Data Structure</Text>
                 <div className="tremor-TextInput-root flex flex-col mt-2 relative w-full items-center min-w-[10rem] outline-none rounded-tremor-default shadow-tremor-input dark:shadow-dark-tremor-input bg-tremor-background dark:bg-dark-tremor-background hover:bg-tremor-background-muted dark:hover:bg-dark-tremor-background-muted text-tremor-content dark:text-dark-tremor-content border-tremor-border dark:border-dark-tremor-border border">
                   <textarea
-                    value={inputValuesForSigning.dataStructure}
+                    value={inputValuesForSigning.dataTypes}
                     onChange={(e) => {
                       e.target.style.height = ""
                       e.target.style.height = e.target.scrollHeight + "px"
-                      setInputValuesForSigning({ ...inputValuesForSigning, dataStructure: e.target.value })
+                      setInputValuesForSigning({ ...inputValuesForSigning, dataTypes: e.target.value })
                     }}
                     // error={!!validationErrors?.userAbi}
                     // errorMessage={validationErrors?.userAbi}
@@ -251,18 +243,18 @@ export default function TestPage() {
             </div>
             <div className="flex flex-col lg:flex-row gap-6 mt-4">
               <div className="flex flex-col grow">
-                <Text>Data Types</Text>
+                <Text>Message</Text>
                 <div className="tremor-TextInput-root flex flex-col mt-2 relative w-full items-center min-w-[10rem] outline-none rounded-tremor-default shadow-tremor-input dark:shadow-dark-tremor-input bg-tremor-background dark:bg-dark-tremor-background hover:bg-tremor-background-muted dark:hover:bg-dark-tremor-background-muted text-tremor-content dark:text-dark-tremor-content border-tremor-border dark:border-dark-tremor-border border">
                   <textarea
-                    value={inputValuesForSigning.dataTypes}
+                    value={inputValuesForSigning.message}
                     onChange={(e) => {
                       e.target.style.height = ""
                       e.target.style.height = e.target.scrollHeight + "px"
-                      setInputValuesForSigning({ ...inputValuesForSigning, dataTypes: e.target.value })
+                      setInputValuesForSigning({ ...inputValuesForSigning, message: e.target.value })
                     }}
                     // error={!!validationErrors?.userAbi}
                     // errorMessage={validationErrors?.userAbi}
-                    placeholder="Copy and paste your data types structure here"
+                    placeholder="Enter your message here"
                     spellCheck={false}
                     className="tremor-TextInput-input w-full focus:outline-none focus:ring-0 border-none bg-transparent text-tremor-default text-tremor-content-emphasis dark:text-dark-tremor-content-emphasis [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none pl-3 pr-4 py-2 placeholder:text-tremor-content dark:placeholder:text-dark-tremor-content resize-none"
                   ></textarea>
@@ -296,7 +288,7 @@ export default function TestPage() {
           </div>
         ) : (
           <div className="flex flex-row md:justify-end mt-4">
-            <Button className="grow md:grow-0" onClick={combineDataAndTypesJson} variant="secondary">
+            <Button className="grow md:grow-0" onClick={parseSigningInputs} variant="secondary">
               Parse
             </Button>
           </div>
