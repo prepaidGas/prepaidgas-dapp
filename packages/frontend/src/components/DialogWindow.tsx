ab348a3976e84415c77b76e25e6c752e1bf2e684
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-02-01 23:03:58 +0200

    userAgreement and DialogWindow minor improvements

--- packages/frontend/src/components/DialogWindow.tsx
+++ packages/frontend/src/components/DialogWindow.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { ReactNode } from "react"
 
-import { Card, Divider, Icon, Metric, Title, Subtitle } from "@tremor/react"
+import { Card, Divider, Icon, Metric, Title, Subtitle, Text } from "@tremor/react"
 import { WalletIcon, XCircleIcon } from "@heroicons/react/24/outline"
 
 interface DialogWindowProps {
@@ -23,7 +23,7 @@ export default function DialogWindow({
 }: DialogWindowProps) {
   return (
     <div className="fixed left-0 top-0 w-full h-full bg-black/80 z-50 flex justify-center items-center p-10">
-      <div className="w-full h-full flex flex-col justify-center items-center md:w-auto md:h-auto ">
+      <div className="w-full h-full flex flex-col justify-center items-center md:w-auto md:h-auto md:max-w-lg">
         <Card className="pt-10 pr-10" decoration="top" decorationColor="orange">
           {isClosable ? (
             <Icon onClick={onClose} className="absolute right-1 top-1" size="lg" icon={XCircleIcon}></Icon>
@@ -32,11 +32,7 @@ export default function DialogWindow({
           {typeof title === "object" ? title : <Title>{title}</Title>}
 
           <Divider />
-          {withoutDescription ? null : typeof description === "object" ? (
-            description
-          ) : (
-            <Subtitle> {description}</Subtitle>
-          )}
+          {withoutDescription ? null : typeof description === "object" ? description : <Text> {description}</Text>}
           {withoutDescription ? null : <Divider />}
           <div className="flex flex-col gap-2">{actionButtons}</div>
         </Card>
--- packages/frontend/src/components/UserAgreement.tsx
+++ packages/frontend/src/components/UserAgreement.tsx
@@ -5,10 +5,11 @@ import CustomConnectBttn from "./CustomConnectBttn"
 
 export default function UserAgreement() {
   const [isTosAccepted, setIsTosAccepted] = useState(false)
+  const [isDisclaimerAccepted, setIsDisclaimerAccepted] = useState(false)
 
   return (
     <div>
-      <div className="flex flex-col mt-4">
+      <div className="flex flex-col">
         <div className="flex flex-row gap-2">
           <label className="w-auto h-[31px] flex flex-row justify-center items-center gap-1">
             <input
@@ -16,6 +17,7 @@ export default function UserAgreement() {
               checked={isTosAccepted}
               type="checkbox"
               name="tos-checkbox"
+              className="mr-2"
             />{" "}
             I have read and accepted the{" "}
             <Link className="link" href={"/tos"} target="_blank">
@@ -23,9 +25,24 @@ export default function UserAgreement() {
             </Link>
           </label>
         </div>
+        <div className="flex flex-row gap-2">
+          <label className="w-auto h-[31px] flex flex-row justify-center items-center gap-1">
+            <input
+              onClick={() => setIsDisclaimerAccepted(!isDisclaimerAccepted)}
+              checked={isDisclaimerAccepted}
+              type="checkbox"
+              name="disclaimer-checkbox"
+              className="mr-2"
+            />{" "}
+            I have read and accepted the{" "}
+            <Link className="link" href={"/disclaimer"} target="_blank">
+              disclaimer
+            </Link>
+          </label>
+        </div>
         {/* {isTosAccepted ? <div className="flex mt-4 items-center justify-center">{children}</div> : null} */}
         <div className="flex mt-4 items-center justify-center">
-          <CustomConnectBttn isActive={isTosAccepted} />
+          <CustomConnectBttn isActive={isTosAccepted && isDisclaimerAccepted} />
         </div>
       </div>
     </div>
