5a1a0f357924c363417913799b4bf2d6cc98a1cb
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-02-01 17:22:25 +0200

    feat: Add Tos and Disclaimer pages

---
+++ packages/frontend/src/app/(legal)/disclaimer/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { Title, Text } from "@tremor/react"
+
+export default function Disclaimer() {
+  return (
+    <div>
+      <Title>Disclaimer</Title>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam autem perspiciatis, inventore, nihil illo iure
+        reprehenderit exercitationem delectus optio cupiditate aliquam, iste dolorum distinctio! Ex deleniti illum nemo
+        ipsum corrupti?
+      </Text>
+    </div>
+  )
+}
---
+++ packages/frontend/src/app/(legal)/tos/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { Title, Text } from "@tremor/react"
+
+export default function TermsOfService() {
+  return (
+    <div>
+      <Title>Terms of service</Title>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam autem perspiciatis, inventore, nihil illo iure
+        reprehenderit exercitationem delectus optio cupiditate aliquam, iste dolorum distinctio! Ex deleniti illum nemo
+        ipsum corrupti?
+      </Text>
+    </div>
+  )
+}
