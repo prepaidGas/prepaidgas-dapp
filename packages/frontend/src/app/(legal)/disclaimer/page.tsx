a404b519c840fa3cbb9a1460cf0c8a8b75729f23
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-02-01 19:54:46 +0200

    fix: tos and disclaimer text margin

--- packages/frontend/src/app/(legal)/disclaimer/page.tsx
+++ packages/frontend/src/app/(legal)/disclaimer/page.tsx
@@ -6,7 +6,7 @@ export default function Disclaimer() {
   return (
     <div>
       <Title>Disclaimer</Title>
-      <Text>
+      <Text className="mt-4">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam autem perspiciatis, inventore, nihil illo iure
         reprehenderit exercitationem delectus optio cupiditate aliquam, iste dolorum distinctio! Ex deleniti illum nemo
         ipsum corrupti?
--- packages/frontend/src/app/(legal)/tos/page.tsx
+++ packages/frontend/src/app/(legal)/tos/page.tsx
@@ -6,7 +6,7 @@ export default function TermsOfService() {
   return (
     <div>
       <Title>Terms of service</Title>
-      <Text>
+      <Text className="mt-4">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam autem perspiciatis, inventore, nihil illo iure
         reprehenderit exercitationem delectus optio cupiditate aliquam, iste dolorum distinctio! Ex deleniti illum nemo
         ipsum corrupti?
