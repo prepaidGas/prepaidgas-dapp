a05227b0719bf205bf387db949d74c568afc101d
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-02-03 18:36:40 +0200

    move contract functions to contract,utils.ts

---
+++ packages/frontend/src/utils/contracts.utils.ts
@@ -0,0 +1,38 @@
+import { readContract, writeContract, waitForTransaction } from "@wagmi/core"
+import { ABIEntry, GasOrderABI, MockTokenABI } from "helpers/abi"
+
+export const readCustomContract = async (address: `0x${string}`, abi: ABIEntry[], functionName: string, args: []) => {
+  console.log("ReadCustomContract args: ", { address, abi, functionName, args })
+
+  try {
+    const data = await readContract({
+      address: address,
+      abi: abi,
+      functionName: functionName,
+      args: args,
+    })
+    console.log("ReadCustomContract result: ", { data })
+    return { ok: true, data, error: undefined }
+  } catch (e) {
+    console.log("ReadCustomContract ERROR: ", { e })
+    return { ok: false, data: undefined, error: e }
+  }
+}
+
+export const writeCustomContract = async (address: `0x${string}`, abi: ABIEntry[], functionName: string, args: []) => {
+  console.log("ReadCustomContract args: ", { address, abi, functionName, args })
+
+  try {
+    const data = await readContract({
+      address: address,
+      abi: abi,
+      functionName: functionName,
+      args: args,
+    })
+    console.log("ReadCustomContract result: ", { data })
+    return { ok: true, data, error: undefined }
+  } catch (e) {
+    console.log("ReadCustomContract ERROR: ", { e })
+    return { ok: false, data: undefined, error: e }
+  }
+}
