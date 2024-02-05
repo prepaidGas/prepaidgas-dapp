dedde36a9ce2279838954745b6c2df2ed6b44a98
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-02-05 17:54:31 +0200

    createOrderCardAdvanced fill WIP

--- packages/frontend/src/components/CreateOrderCards/CreateOrderCardAdvanced.tsx
+++ packages/frontend/src/components/CreateOrderCards/CreateOrderCardAdvanced.tsx
@@ -26,29 +26,32 @@ import {
   Icon,
   Button,
   DatePickerValue,
+  SearchSelect,
+  SearchSelectItem,
 } from "@tremor/react"
 import { TailSpin } from "react-loader-spinner"
 import { Dispatch, SetStateAction, useEffect, useState } from "react"
 import { ETH_ADDRESS_REGEX, TIME_STRING_REGEX, SPINNER_COLOR } from "@/constants"
 import { z } from "zod"
 import { CreateOrderState } from "./CreateOrderCard"
+import Receipt from "../Receipt"
+import { getGuaranteeValue, getRewardValue } from "@/utils/utils"
 
 export default function CreateOrderCardAdvanced({
-  setShowDialogWindow,
-  setTransactionDetails,
   setInputValues,
   inputValues,
   validationErrors,
   handleSubmit,
 }: {
-  setShowDialogWindow: Dispatch<SetStateAction<boolean>>
-  setTransactionDetails: Dispatch<SetStateAction<{}>>
   setInputValues: Dispatch<SetStateAction<{}>>
   inputValues: CreateOrderState
   validationErrors: null | { [key: string]: string }
   handleSubmit: () => void
 }) {
   const [isLoading, setIsLoading] = useState(false)
+  const [wasGasPriceChanged, setWasGasPriceChanged] = useState(false)
+  const [wasRewardValueChanged, setWasRewardValueChanged] = useState(false)
+  const [wasGuaranteeValueChanged, setWasGuaranteeValueChanged] = useState(false)
   const [wasRewardTransferChanged, setWasRewardTransferChanged] = useState(false)
   const [wasGasCostTransferChanged, setWasGasCostTransferChanged] = useState(false)
 
@@ -77,9 +80,28 @@ export default function CreateOrderCardAdvanced({
             <NumberInput
               icon={FireIcon}
               value={inputValues.gasAmount.toString()}
-              onChange={(e) =>
-                setInputValues({ ...inputValues, gasAmount: clampNumber(Number(e.target.value), 0, 100000) })
-              }
+              //onChange={(e) => setInputValues({ ...inputValues, gasAmount: Number(e.target.value) })}
+              onChange={(e) => {
+                setInputValues({ ...inputValues, gasAmount: Number(e.target.value) })
+                const gasAmount = Number(e.target.value)
+                if (
+                  wasGasPriceChanged === false &&
+                  wasRewardValueChanged === false &&
+                  wasGuaranteeValueChanged === false &&
+                  wasGasCostTransferChanged === false &&
+                  wasRewardTransferChanged === false
+                ) {
+                  setInputValues({
+                    ...inputValues,
+                    gasAmount,
+                    guaranteeValueGasPrice: getGuaranteeValue(gasAmount, inputValues.gasCostValueGasPrice),
+                    rewardValueAmount: getRewardValue(gasAmount, inputValues.gasCostValueGasPrice),
+                    gasCostTransfer: getGuaranteeValue(gasAmount, inputValues.gasCostValueGasPrice),
+                    rewardTransfer: getRewardValue(gasAmount, inputValues.gasCostValueGasPrice),
+                  })
+                }
+              }}
+              min={0}
               error={!!validationErrors?.gasAmount}
               errorMessage={validationErrors?.gasAmount}
               spellCheck={false}
@@ -135,15 +157,23 @@ export default function CreateOrderCardAdvanced({
       <div className="flex flex-col mt-4 lg:flex-row gap-6">
         <div className="flex flex-col lg:grow">
           <Text>Reward Token</Text>
-          <TextInput
+          <SearchSelect
             className="mt-2"
             value={inputValues.rewardValueToken}
-            onChange={(e) => setInputValues({ ...inputValues, rewardValueToken: e.target.value })}
-            error={!!validationErrors?.rewardValueToken}
-            errorMessage={validationErrors?.rewardValueToken}
+            onValueChange={(value) =>
+              setInputValues({
+                ...inputValues,
+                rewardValueToken: value,
+              })
+            }
             spellCheck={false}
             placeholder="0x1dA..."
-          />
+          >
+            <SearchSelectItem value="Test0">Test0</SearchSelectItem>
+            <SearchSelectItem value="Test1">Test1</SearchSelectItem>
+            <SearchSelectItem value="Test2">Test2</SearchSelectItem>
+            <SearchSelectItem value="Test3">Test3</SearchSelectItem>
+          </SearchSelect>
         </div>
         <div className="flex flex-col lg:grow">
           <Text>Reward Amount</Text>
@@ -151,7 +181,7 @@ export default function CreateOrderCardAdvanced({
             className="mt-2"
             value={inputValues.rewardValueAmount.toString()}
             onChange={(e) => {
-              const rewardValueAmount = clampNumber(Number(e.target.value), 0, 100000)
+              const rewardValueAmount = Number(e.target.value)
               wasRewardTransferChanged
                 ? setInputValues({ ...inputValues, rewardValueAmount })
                 : setInputValues({ ...inputValues, rewardValueAmount, rewardTransfer: rewardValueAmount })
@@ -167,15 +197,23 @@ export default function CreateOrderCardAdvanced({
       <div className="flex flex-col mt-4 lg:flex-row gap-6">
         <div className="flex flex-col lg:grow">
           <Text>Gas Cost Token</Text>
-          <TextInput
+          <SearchSelect
             className="mt-2"
             value={inputValues.gasCostValueToken}
-            onChange={(e) => setInputValues({ ...inputValues, gasCostValueToken: e.target.value })}
-            error={!!validationErrors?.gasCostValueToken}
-            errorMessage={validationErrors?.gasCostValueToken}
+            onValueChange={(value) =>
+              setInputValues({
+                ...inputValues,
+                gasCostValueToken: value,
+              })
+            }
             spellCheck={false}
             placeholder="0x1dA..."
-          />
+          >
+            <SearchSelectItem value="Test0">Test0</SearchSelectItem>
+            <SearchSelectItem value="Test1">Test1</SearchSelectItem>
+            <SearchSelectItem value="Test2">Test2</SearchSelectItem>
+            <SearchSelectItem value="Test3">Test3</SearchSelectItem>
+          </SearchSelect>
         </div>
         <div className="flex flex-col lg:grow">
           <Text>Gas Cost GasPrice</Text>
@@ -206,15 +244,23 @@ export default function CreateOrderCardAdvanced({
       <div className="flex flex-col mt-4 lg:flex-row gap-6">
         <div className="flex flex-col lg:grow">
           <Text>Guarantee Token</Text>
-          <TextInput
-            className=" mt-2"
+          <SearchSelect
+            className="mt-2"
             value={inputValues.guaranteeValueToken}
-            onChange={(e) => setInputValues({ ...inputValues, guaranteeValueToken: e.target.value })}
-            error={!!validationErrors?.guaranteeValueToken}
-            errorMessage={validationErrors?.guaranteeValueToken}
+            onValueChange={(value) =>
+              setInputValues({
+                ...inputValues,
+                guaranteeValueToken: value,
+              })
+            }
             spellCheck={false}
             placeholder="0x1dA..."
-          />
+          >
+            <SearchSelectItem value="Test0">Test0</SearchSelectItem>
+            <SearchSelectItem value="Test1">Test1</SearchSelectItem>
+            <SearchSelectItem value="Test2">Test2</SearchSelectItem>
+            <SearchSelectItem value="Test3">Test3</SearchSelectItem>
+          </SearchSelect>
         </div>
         <div className="flex flex-col lg:grow">
           <Text>Guarantee GasPrice</Text>
@@ -232,55 +278,56 @@ export default function CreateOrderCardAdvanced({
       </div>
 
       {/* Advanced Settings */}
-      <Accordion className="mt-4 overflow-visible">
-        <AccordionHeader>Advanced Settings</AccordionHeader>
-        <AccordionBody className="flex flex-col gap-2">
-          <div className="flex flex-col lg:flex-row gap-6">
-            <div className="flex flex-col">
-              <Text>Execution window</Text>
-              <NumberInput
-                className="mt-2"
-                value={inputValues.executionWindow.toString()}
-                onChange={(e) =>
-                  setInputValues({ ...inputValues, executionWindow: clampNumber(Number(e.target.value), 0, 100000) })
-                }
-                error={!!validationErrors?.executionWindow}
-                errorMessage={validationErrors?.executionWindow}
-                spellCheck={false}
-              />
-            </div>
-            <div className="flex flex-col ">
-              <Text>Reward transfer</Text>
-              <NumberInput
-                className="mt-2"
-                value={inputValues.rewardTransfer.toString()}
-                onChange={(e) => {
-                  setInputValues({ ...inputValues, rewardTransfer: clampNumber(Number(e.target.value), 0, 100000) })
-                  setWasRewardTransferChanged(true)
-                }}
-                error={!!validationErrors?.rewardTransfer}
-                errorMessage={validationErrors?.rewardTransfer}
-                spellCheck={false}
-              />
-            </div>
-            <div className="flex flex-col ">
-              <Text>Gas cost transfer</Text>
-              <NumberInput
-                className="mt-2"
-                value={inputValues.gasCostTransfer.toString()}
-                onChange={(e) => {
-                  setInputValues({ ...inputValues, gasCostTransfer: clampNumber(Number(e.target.value), 0, 100000) })
-                  setWasGasCostTransferChanged(true)
-                }}
-                error={!!validationErrors?.gasCostTransfer}
-                errorMessage={validationErrors?.gasCostTransfer}
-                spellCheck={false}
-              />
-            </div>
-          </div>
-        </AccordionBody>
-      </Accordion>
-      <div className="flex flex-row md:justify-end mt-4">
+      <div className="flex flex-col lg:flex-row gap-6 mt-4">
+        <div className="flex flex-col">
+          <Text>Execution window</Text>
+          <NumberInput
+            className="mt-2"
+            value={inputValues.executionWindow.toString()}
+            onChange={(e) =>
+              setInputValues({ ...inputValues, executionWindow: clampNumber(Number(e.target.value), 0, 100000) })
+            }
+            error={!!validationErrors?.executionWindow}
+            errorMessage={validationErrors?.executionWindow}
+            spellCheck={false}
+          />
+        </div>
+        <div className="flex flex-col ">
+          <Text>Reward transfer</Text>
+          <NumberInput
+            className="mt-2"
+            value={inputValues.rewardTransfer.toString()}
+            onChange={(e) => {
+              setInputValues({ ...inputValues, rewardTransfer: clampNumber(Number(e.target.value), 0, 100000) })
+              setWasRewardTransferChanged(true)
+            }}
+            error={!!validationErrors?.rewardTransfer}
+            errorMessage={validationErrors?.rewardTransfer}
+            spellCheck={false}
+          />
+        </div>
+        <div className="flex flex-col ">
+          <Text>Gas cost transfer</Text>
+          <NumberInput
+            className="mt-2"
+            value={inputValues.gasCostTransfer.toString()}
+            onChange={(e) => {
+              setInputValues({ ...inputValues, gasCostTransfer: clampNumber(Number(e.target.value), 0, 100000) })
+              setWasGasCostTransferChanged(true)
+            }}
+            error={!!validationErrors?.gasCostTransfer}
+            errorMessage={validationErrors?.gasCostTransfer}
+            spellCheck={false}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 md:gap-0 md:items-end md:flex-row md:justify-between mt-4">
+        <Receipt
+          //TODO pass correct token names
+          gasAmount={inputValues.gasAmount}
+          gasCostValue={inputValues.gasCostValueGasPrice}
+          rewardValue={inputValues.rewardValueAmount}
+        />
         <Button className="grow md:grow-0" disabled={isLoading} onClick={handleSubmit}>
           {/* <Button onClick={() => setIsLoading(!isLoading)}> */}
           <TailSpin
