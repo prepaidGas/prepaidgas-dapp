82f1b875cc8118d9b36710b041aeadc596df5c45
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-02-06 16:08:23 +0200

    change receipt component to use List instead of callout card

--- packages/frontend/src/components/Receipt.tsx
+++ packages/frontend/src/components/Receipt.tsx
@@ -1,5 +1,5 @@
 import { DocumentTextIcon } from "@heroicons/react/24/outline"
-import { Callout, Text } from "@tremor/react"
+import { Callout, Icon, List, ListItem, Text, Title } from "@tremor/react"
 import Tooltip from "./Tooltip"
 
 export default function Receipt({
@@ -15,27 +15,56 @@ export default function Receipt({
   rewardTokenName?: string
   rewardValue: number
 }) {
+  // return (
+  //   <Callout className="flex flex-col" title="Receipt" icon={DocumentTextIcon} color="teal">
+  //     <div className="flex flex-row justify-start items-center gap-2">
+  //       <div className="flex flex-col">
+  //         {gasCostTokenName === rewardTokenName ? (
+  //           <Text>
+  //             {rewardValue + gasCostValue * gasAmount} {gasCostTokenName}
+  //           </Text>
+  //         ) : (
+  //           <>
+  //             <Text>
+  //               {gasCostValue * gasAmount} {gasCostTokenName}
+  //             </Text>
+  //             <Text>
+  //               {rewardValue} {rewardTokenName}
+  //             </Text>
+  //           </>
+  //         )}
+  //       </div>
+  //       <Tooltip tooltipText="This is just a placeholder" iconSize="lg" />
+  //     </div>
+  //   </Callout>
+  // )
+
   return (
-    <Callout className="flex flex-col" title="Receipt" icon={DocumentTextIcon} color="teal">
-      <div className="flex flex-row justify-start items-center gap-2">
-        <div className="flex flex-col">
-          {gasCostTokenName === rewardTokenName ? (
-            <Text>
-              {rewardValue + gasCostValue * gasAmount} {gasCostTokenName}
-            </Text>
-          ) : (
-            <>
-              <Text>
-                {gasCostValue * gasAmount} {gasCostTokenName}
-              </Text>
-              <Text>
-                {rewardValue} {rewardTokenName}
-              </Text>
-            </>
-          )}
-        </div>
-        <Tooltip tooltipText="This is just a placeholder" iconSize="lg" />
+    <div className="relative">
+      {/* <div className="absolute right-[-2rem] top-0">
+        <Tooltip tooltipText="This is just a placeholder" iconSize="md" />
+      </div> */}
+      <div className="flex flex-row gap-1 items-center">
+        {/* <Icon icon={DocumentTextIcon} /> */}
+        <Title>Receipt</Title>
+        <Tooltip tooltipText="This is just a placeholder" iconSize="md" />
       </div>
-    </Callout>
+      <List className="flex flex-col" title="Receipt">
+        {gasCostTokenName === rewardTokenName ? (
+          <ListItem>
+            {rewardValue + gasCostValue * gasAmount} {gasCostTokenName}
+          </ListItem>
+        ) : (
+          <>
+            <ListItem>
+              {gasCostValue * gasAmount} {gasCostTokenName}
+            </ListItem>
+            <ListItem>
+              {rewardValue} {rewardTokenName}
+            </ListItem>
+          </>
+        )}
+      </List>
+    </div>
   )
 }
