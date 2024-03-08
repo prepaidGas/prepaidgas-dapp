5107ed949ce1f3cd2494f5878ca757c41b7fa2b0
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-03-08 19:23:31 +0200

    refactor: change createOrderCard to use antd

--- packages/frontend/src/components/CreateOrderCards/CreateOrderCardSimple.tsx
+++ packages/frontend/src/components/CreateOrderCards/CreateOrderCardSimple.tsx
@@ -11,6 +11,12 @@ import { getGuaranteeValue, getRewardValue } from "@/utils/utils"
 import TokenSearchSelect from "../TokenSearchSelect"
 import { TOKEN_NAME } from "@/constants/tokens"
 
+import { Form, Input, List, Select, Tabs, TabsProps } from "antd"
+const { Option } = Select
+
+import { Buttons } from "@/components/buttons"
+import CreateOrderCard from "@/components/CreateOrderCards/CreateOrderCard"
+
 export default function CreateOrderCardSimple({
   setInputValues,
   inputValues,
@@ -28,90 +34,98 @@ export default function CreateOrderCardSimple({
     <div className="mt-6 flex flex-col w-full">
       {/* Gas Amount, Token and Gas Price inputs */}
       <div className="flex flex-col lg:flex-row gap-6">
-        <div className="flex flex-col">
-          <Text>Gas</Text>
-          <div className="flex flex-col mt-2">
-            <NumberInput
-              icon={FireIcon}
-              value={inputValues.gasAmount.toString()}
-              onChange={(e) => {
-                const gasAmount = Number(e.target.value)
-                setInputValues({
-                  ...inputValues,
-                  gasAmount,
-                  guaranteeValueGasPrice: getGuaranteeValue(gasAmount, inputValues.gasCostValueGasPrice),
-                  rewardValueAmount: getRewardValue(gasAmount, inputValues.gasCostValueGasPrice),
-                  gasCostTransfer: getGuaranteeValue(gasAmount, inputValues.gasCostValueGasPrice),
-                  rewardTransfer: getRewardValue(gasAmount, inputValues.gasCostValueGasPrice),
-                })
-              }}
-              min={0}
-              error={!!validationErrors?.gasAmount}
-              errorMessage={validationErrors?.gasAmount}
-              spellCheck={false}
-            ></NumberInput>
+        <Form>
+          <div className="flex flex-col">
+            <label htmlFor="input-number-gas" className="text-[#404040] dark:text-[#A4A5AA]">
+              Gas Amount
+            </label>
+            <Form.Item name="input-number-gas">
+              <Input
+                value={inputValues.gasAmount.toString()}
+                onChange={(e) => {
+                  const gasAmount = Number(e.target.value)
+                  setInputValues({
+                    ...inputValues,
+                    gasAmount,
+                    guaranteeValueGasPrice: getGuaranteeValue(gasAmount, inputValues.gasCostValueGasPrice),
+                    rewardValueAmount: getRewardValue(gasAmount, inputValues.gasCostValueGasPrice),
+                    gasCostTransfer: getGuaranteeValue(gasAmount, inputValues.gasCostValueGasPrice),
+                    rewardTransfer: getRewardValue(gasAmount, inputValues.gasCostValueGasPrice),
+                  })
+                }}
+                min={0}
+                // error={!!validationErrors?.gasAmount}
+                // errorMessage={validationErrors?.gasAmount}
+                spellCheck={false}
+                placeholder="123"
+                size="middle"
+                className="h-12 p-3 rounded-6 border-normal dark:border-whiteDark hover:border-primary focus:border-primary dark:placeholder-white/60"
+              />
+            </Form.Item>
           </div>
-        </div>
-        <div className="flex flex-col lg:grow">
-          <Text>Gas Cost Token</Text>
-          <TokenSearchSelect
-            className="mt-2"
-            searchSelectValue={inputValues.gasCostValueToken}
-            changeHandler={(value) =>
-              setInputValues({
-                ...inputValues,
-                gasCostValueToken: value,
-                guaranteeValueToken: value,
-                rewardValueToken: value,
-              })
-            }
-          />
-        </div>
-        <div className="flex flex-col lg:grow">
-          <Text>Gas Cost GasPrice</Text>
-          <NumberInput
-            className="mt-2"
-            value={inputValues.gasCostValueGasPrice.toString()}
-            onChange={(e) => {
-              const gasCostValueGasPrice = Number(e.target.value)
-              setInputValues({
-                ...inputValues,
-                gasCostValueGasPrice,
-                guaranteeValueGasPrice: getGuaranteeValue(inputValues.gasAmount, gasCostValueGasPrice),
-                rewardValueAmount: getRewardValue(inputValues.gasAmount, gasCostValueGasPrice),
-                gasCostTransfer: getGuaranteeValue(inputValues.gasAmount, gasCostValueGasPrice),
-                rewardTransfer: getRewardValue(inputValues.gasAmount, gasCostValueGasPrice),
-              })
-            }}
-            error={!!validationErrors?.gasCostValueGasPrice}
-            errorMessage={validationErrors?.gasCostValueGasPrice}
-            spellCheck={false}
-          />
-        </div>
+          <div className="flex flex-col grow">
+            <label htmlFor="token-select" className="text-[#404040] dark:text-[#A4A5AA]">
+              Token
+            </label>
+            <Form.Item name="token-select">
+              <TokenSearchSelect
+                className="mt-2"
+                searchSelectValue={inputValues.gasCostValueToken}
+                changeHandler={(value) =>
+                  setInputValues({
+                    ...inputValues,
+                    gasCostValueToken: value,
+                    guaranteeValueToken: value,
+                    rewardValueToken: value,
+                  })
+                }
+              />
+            </Form.Item>
+          </div>
+          <div className="flex flex-col grow">
+            <label htmlFor="input-number-gasCost" className="text-[#404040] dark:text-[#A4A5AA]">
+              Gas Cost
+            </label>
+            <Form.Item name="input-number-gasCost">
+              <Input
+                value={inputValues.gasCostValueGasPrice.toString()}
+                onChange={(e) => {
+                  const gasCostValueGasPrice = Number(e.target.value)
+                  setInputValues({
+                    ...inputValues,
+                    gasCostValueGasPrice,
+                    guaranteeValueGasPrice: getGuaranteeValue(inputValues.gasAmount, gasCostValueGasPrice),
+                    rewardValueAmount: getRewardValue(inputValues.gasAmount, gasCostValueGasPrice),
+                    gasCostTransfer: getGuaranteeValue(inputValues.gasAmount, gasCostValueGasPrice),
+                    rewardTransfer: getRewardValue(inputValues.gasAmount, gasCostValueGasPrice),
+                  })
+                }}
+                // error={!!validationErrors?.gasCostValueGasPrice}
+                // errorMessage={validationErrors?.gasCostValueGasPrice}
+                spellCheck={false}
+                placeholder="123"
+                size="middle"
+                className="h-12 p-3 rounded-6 border-normal dark:border-whiteDark hover:border-primary focus:border-primary dark:placeholder-white/60"
+              />
+            </Form.Item>
+          </div>
+        </Form>
       </div>
       <div className="flex flex-col gap-4 md:gap-0 md:items-end md:flex-row md:justify-between mt-4">
-        <Receipt
+        {/* <Receipt
           //TODO pass correct token names
           gasAmount={inputValues.gasAmount}
           gasCostValue={inputValues.gasCostValueGasPrice}
           rewardValue={inputValues.rewardValueAmount}
           gasCostTokenName={TOKEN_NAME[inputValues.gasCostValueToken] ?? inputValues.gasCostValueToken}
           rewardTokenName={TOKEN_NAME[inputValues.rewardValueToken] ?? inputValues.rewardValueToken}
-        />
-        <Button className="grow md:grow-0" disabled={isLoading} onClick={handleSubmit}>
-          {/* <Button onClick={() => setIsLoading(!isLoading)}> */}
-          <TailSpin
-            height={20}
-            width={20}
-            color={SPINNER_COLOR}
-            ariaLabel="tail-spin-loading"
-            radius="0"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={isLoading}
-          />
-          {isLoading ? "" : "Create Gas Order"}
-        </Button>
+        /> */}
+        <Buttons
+          onClick={handleSubmit}
+          className="grow md:grow-0 bg-primary hover:bg-primary-hbr border-solid border-1 border-primary text-white dark:text-white/[.87] text-[14px] font-semibold leading-[22px] inline-flex items-center justify-center rounded-[4px] px-[20px] h-[44px]"
+        >
+          {"Create Gas Order"}
+        </Buttons>
       </div>
     </div>
   )
