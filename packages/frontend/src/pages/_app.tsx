8355c001808aa6e56bf2f9ee6955955840eccfe7
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-03-08 19:11:35 +0200

    add wagmi config and rainbowkitprovider

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,23 +1,55 @@
-import '@/styles/globals.css'
-import { useRouter } from 'next/router';
-import type { AppProps } from 'next/app';
+import "@/styles/globals.css"
+import { useRouter } from "next/router"
+import type { AppProps } from "next/app"
 
-import { Provider } from "react-redux";
-import { UserProvider } from '@auth0/nextjs-auth0/client';
-import AdminLayout from './adminLayout';
-import AuthLayout from './authLayout';
-import { wrapper, store } from "../redux/store";
-import '../i18n/config';
+import { Provider } from "react-redux"
+import { UserProvider } from "@auth0/nextjs-auth0/client"
+import AdminLayout from "./adminLayout"
+import AuthLayout from "./authLayout"
+import { wrapper, store } from "../redux/store"
+import "../i18n/config"
 
-import { AuthContextProvider } from '../authentication/AuthContext';
+import { AuthContextProvider } from "../authentication/AuthContext"
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit"
+import { WagmiConfig, configureChains, createConfig } from "wagmi"
+import { hardhat, mainnet } from "wagmi/chains"
+import { publicProvider } from "wagmi/providers/public"
+import { ConnectButton, DisclaimerComponent, getDefaultWallets, lightTheme } from "@rainbow-me/rainbowkit"
 
+const { chains, publicClient } = configureChains([mainnet, hardhat], [publicProvider()])
+
+const { connectors } = getDefaultWallets({
+  appName: "PrepaidGas",
+  projectId: "PREPAIDGAS",
+  chains,
+})
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+})
+
+const url = "http://localhost:3000"
+
+const Disclaimer: DisclaimerComponent = ({ Text, Link }) => (
+  <Text>
+    By connecting your wallet, you agree to the <Link href={`${url}/tos`}>Terms of Service</Link> and acknowledge you
+    have read and understand the protocol <Link href={`${url}/disclaimer`}>Disclaimer</Link>
+  </Text>
+)
 
 function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const { pathname } = router;
+  const router = useRouter()
+  const { pathname } = router
 
   const renderLayout = () => {
-    if (pathname=='/' || pathname.startsWith('/register') || pathname.startsWith('/forgotPassword') || pathname.startsWith('/login')) {
+    if (
+      pathname == "/" ||
+      pathname.startsWith("/register") ||
+      pathname.startsWith("/forgotPassword") ||
+      pathname.startsWith("/login")
+    ) {
       return (
         <UserProvider profileUrl="/hexadash-nextjs/api/auth/me">
           <AuthContextProvider>
@@ -26,29 +58,37 @@ function App({ Component, pageProps }: AppProps) {
             </AuthLayout>
           </AuthContextProvider>
         </UserProvider>
-      );
-    }
-    else {
+      )
+    } else {
       return (
         <UserProvider profileUrl="/hexadash-nextjs/api/auth/me">
           <AuthContextProvider>
             <AdminLayout>
-              <Component {...pageProps} />
+              <WagmiConfig config={wagmiConfig}>
+                <RainbowKitProvider
+                  chains={chains}
+                  appInfo={{
+                    appName: "PrepaidGas",
+                    disclaimer: Disclaimer,
+                  }}
+                  //todo: decide wether to use theme attribute cuz it wraps all page in <div data-rk> which causes css problems
+                  // theme={lightTheme({ accentColor: "#f97316" })}
+                >
+                  <Component {...pageProps} />
+                </RainbowKitProvider>
+              </WagmiConfig>
             </AdminLayout>
           </AuthContextProvider>
         </UserProvider>
-      );
+      )
     }
-  };
-
+  }
 
   return (
     <>
-      <Provider store={store}>
-        {renderLayout()}
-      </Provider>
+      <Provider store={store}>{renderLayout()}</Provider>
     </>
   )
 }
 
-export default wrapper.withRedux(App);
+export default wrapper.withRedux(App)
