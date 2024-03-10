b29135144102a17798a7c09d7295135a4efc36ba
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-03-10 17:07:02 +0200

    feat: add responsive to advanced order creation

--- packages/frontend/src/components/CreateOrderCards/CreateOrderCardAdvanced.tsx
+++ packages/frontend/src/components/CreateOrderCards/CreateOrderCardAdvanced.tsx
@@ -29,43 +29,8 @@ export default function CreateOrderCardAdvanced({
 
   return (
     <div className="mt-6 flex flex-col w-full">
-      {/* Gas Amount and Date & Time Settings */}
-      <div className="flex flex-col lg:flex-row gap-6">
-        {/* <div className="flex flex-col">
-          <Text>Gas</Text>
-          <div className="flex flex-col mt-2">
-            <NumberInput
-              icon={FireIcon}
-              value={inputValues.gasAmount.toString()}
-              //onChange={(e) => setInputValues({ ...inputValues, gasAmount: Number(e.target.value) })}
-              onChange={(e) => {
-                setInputValues({ ...inputValues, gasAmount: Number(e.target.value) })
-                const gasAmount = Number(e.target.value)
-                if (
-                  wasGasPriceChanged === false &&
-                  wasRewardValueChanged === false &&
-                  wasGuaranteeValueChanged === false &&
-                  wasGasCostTransferChanged === false &&
-                  wasRewardTransferChanged === false
-                ) {
-                  setInputValues({
-                    ...inputValues,
-                    gasAmount,
-                    guaranteeValueGasPrice: getGuaranteeValue(gasAmount, inputValues.gasCostValueGasPrice),
-                    rewardValueAmount: getRewardValue(gasAmount, inputValues.gasCostValueGasPrice),
-                    gasCostTransfer: getGuaranteeValue(gasAmount, inputValues.gasCostValueGasPrice),
-                    rewardTransfer: getRewardValue(gasAmount, inputValues.gasCostValueGasPrice),
-                  })
-                }
-              }}
-              min={0}
-              error={!!validationErrors?.gasAmount}
-              errorMessage={validationErrors?.gasAmount}
-              spellCheck={false}
-            />
-          </div>
-        </div> */}
-        <div className="flex flex-col">
+      <div className="flex flex-col old-lg:flex-row gap-6">
+        <div className="flex flex-col flex-1">
           <label htmlFor="input-number-gas" className="text-[#404040] dark:text-[#A4A5AA] mb-1">
             Gas Amount
           </label>
@@ -101,127 +66,90 @@ export default function CreateOrderCardAdvanced({
             className="h-12 p-3 rounded-6 border-normal dark:border-whiteDark hover:border-primary focus:border-primary dark:placeholder-white/60"
           />
         </div>
-        {/* <div className="flex flex-col justify-between">
-          <Text>Execution period Start</Text>
-          <div className="flex flex-row mt-2">
-            <DatePicker
-              color="red"
-              value={inputValues.executionPeriodStartDate}
-              onValueChange={(value) => setInputValues({ ...inputValues, executionPeriodStartDate: value })}
-              minDate={inputValues.executionPeriodStartDate}
-            />
-          </div>
-          <div className="flex flex-row mt-2">
-            <TextInput
-              icon={ClockIcon}
-              value={inputValues.executionPeriodStartTime}
-              onChange={(e) => setInputValues({ ...inputValues, executionPeriodStartTime: e.target.value })}
-              placeholder={inputValues.executionPeriodStartTime}
-              error={!!validationErrors?.executionPeriodStartTime}
-              errorMessage={validationErrors?.executionPeriodStartTime}
-              spellCheck={false}
-            ></TextInput>
-          </div>
-        </div> */}
-        <div className="flex flex-col gap-4">
+
+        <div className="flex flex-col">
           <label className="text-[#404040] dark:text-[#A4A5AA] mb-1">Execution period Start</label>
-          <DatePicker
-            defaultValue={dayjs().add(0, "d")}
-            // value={dayjs(inputValues.executionPeriodStartDate)}
-            presets={[
-              {
-                label: "Tommorrow",
-                value: dayjs().add(1, "d"),
-              },
-              {
-                label: "Next Week",
-                value: dayjs().add(7, "d"),
-              },
-              {
-                label: "Next Month",
-                value: dayjs().add(1, "month"),
-              },
-            ]}
-            onChange={(date) => {
-              if (date) {
-                // setInputValues({ ...inputValues, executionPeriodStartDate: date })
-              }
-            }}
-          />
-          <TimePicker
-            className="dark:[&>div>input]:text-white/60 dark:[&>div>.ant-picker-suffix]:text-white/60"
-            defaultValue={dayjs("00:00", "HH:mm")}
-            format={"HH:mm"}
-            // value={inputValues.executionPeriodStartTime}
-            // onChange={(value) => setInputValues({ ...inputValues, executionPeriodStartTime: value })}
-            // error={!!validationErrors?.executionPeriodStartTime}
-            // errorMessage={validationErrors?.executionPeriodStartTime}
-          />
-        </div>
-        {/* <div className="flex flex-col justify-between">
-          <Text>Execution period End</Text>
-          <div className="flex flex-row mt-2">
+          <div className="flex flex-col gap-4">
             <DatePicker
-              value={inputValues.executionPeriodEndDate}
-              onValueChange={(value) => setInputValues({ ...inputValues, executionPeriodEndDate: value })}
-              minDate={inputValues.executionPeriodStartDate}
+              defaultValue={dayjs().add(0, "d")}
+              // value={dayjs(inputValues.executionPeriodStartDate)}
+              presets={[
+                {
+                  label: "Tommorrow",
+                  value: dayjs().add(1, "d"),
+                },
+                {
+                  label: "Next Week",
+                  value: dayjs().add(7, "d"),
+                },
+                {
+                  label: "Next Month",
+                  value: dayjs().add(1, "month"),
+                },
+              ]}
+              onChange={(date) => {
+                if (date) {
+                  // setInputValues({ ...inputValues, executionPeriodStartDate: date })
+                }
+              }}
+            />
+            <TimePicker
+              className="dark:[&>div>input]:text-white/60 dark:[&>div>.ant-picker-suffix]:text-white/60"
+              defaultValue={dayjs("00:00", "HH:mm")}
+              format={"HH:mm"}
+              // value={inputValues.executionPeriodStartTime}
+              // onChange={(value) => setInputValues({ ...inputValues, executionPeriodStartTime: value })}
+              // error={!!validationErrors?.executionPeriodStartTime}
+              // errorMessage={validationErrors?.executionPeriodStartTime}
             />
           </div>
-          <div className="flex flex-row mt-2">
-            <TextInput
-              icon={ClockIcon}
-              value={inputValues.executionPeriodEndTime}
-              onChange={(e) => setInputValues({ ...inputValues, executionPeriodEndTime: e.target.value })}
-              placeholder={inputValues.executionPeriodEndTime}
-              error={!!validationErrors?.executionPeriodEndTime}
-              errorMessage={validationErrors?.executionPeriodEndTime}
-              spellCheck={false}
-            ></TextInput>
-          </div>
-        </div> */}
-        <div className="flex flex-col gap-4">
+        </div>
+
+        <div className="flex flex-col">
           <label className="text-[#404040] dark:text-[#A4A5AA] mb-1">Execution period End</label>
-          <DatePicker
-            defaultValue={dayjs().add(0, "d")}
-            // value={dayjs(inputValues.executionPeriodEndDate)}
-            presets={[
-              {
-                label: "Tommorrow",
-                value: dayjs().add(1, "d"),
-              },
-              {
-                label: "Next Week",
-                value: dayjs().add(7, "d"),
-              },
-              {
-                label: "Next Month",
-                value: dayjs().add(1, "month"),
-              },
-            ]}
-            onChange={(date) => {
-              if (date) {
-                // setInputValues({ ...inputValues, executionPeriodEndDate: date })
-              }
-            }}
-          />
-          <TimePicker
-            className="dark:[&>div>input]:text-white/60 dark:[&>div>.ant-picker-suffix]:text-white/60"
-            defaultValue={dayjs("00:00", "HH:mm")}
-            format={"HH:mm"}
-            // value={inputValues.executionPeriodEndTime}
-            // onChange={(value) => setInputValues({ ...inputValues, executionPeriodEndTime: value })}
-            // error={!!validationErrors?.executionPeriodEndTime}
-            // errorMessage={validationErrors?.executionPeriodEndTime}
-          />
+          <div className="flex flex-col gap-4">
+            <DatePicker
+              defaultValue={dayjs().add(0, "d")}
+              // value={dayjs(inputValues.executionPeriodEndDate)}
+              presets={[
+                {
+                  label: "Tommorrow",
+                  value: dayjs().add(1, "d"),
+                },
+                {
+                  label: "Next Week",
+                  value: dayjs().add(7, "d"),
+                },
+                {
+                  label: "Next Month",
+                  value: dayjs().add(1, "month"),
+                },
+              ]}
+              onChange={(date) => {
+                if (date) {
+                  // setInputValues({ ...inputValues, executionPeriodEndDate: date })
+                }
+              }}
+            />
+            <TimePicker
+              className="dark:[&>div>input]:text-white/60 dark:[&>div>.ant-picker-suffix]:text-white/60"
+              defaultValue={dayjs("00:00", "HH:mm")}
+              format={"HH:mm"}
+              // value={inputValues.executionPeriodEndTime}
+              // onChange={(value) => setInputValues({ ...inputValues, executionPeriodEndTime: value })}
+              // error={!!validationErrors?.executionPeriodEndTime}
+              // errorMessage={validationErrors?.executionPeriodEndTime}
+            />
+          </div>
         </div>
       </div>
 
       {/* Reward Settings */}
-      <div className="flex flex-col mt-4 lg:flex-row gap-6">
-        <div className="flex flex-col lg:grow">
+      <div className="flex flex-col mt-4 old-lg:flex-row gap-6">
+        <div className="flex flex-col flex-1">
           <label className="text-[#404040] dark:text-[#A4A5AA] mb-1">Reward Token</label>
           <TokenSearchSelect
-            className="mt-2"
+            className=""
             searchSelectValue={inputValues.rewardValueToken}
             changeHandler={(value) =>
               setInputValues({
@@ -231,22 +159,8 @@ export default function CreateOrderCardAdvanced({
             }
           />
         </div>
-        {/* <div className="flex flex-col lg:grow">
-          <Text>Reward Amount</Text>
-          <NumberInput
-            className="mt-2"
-            value={inputValues.rewardValueAmount.toString()}
-            onChange={(e) => {
-              const rewardValueAmount = Number(e.target.value)
-              setInputValues({ ...inputValues, rewardValueAmount })
-            }}
-            min={0}
-            error={!!validationErrors?.rewardValueAmount}
-            errorMessage={validationErrors?.rewardValueAmount}
-            spellCheck={false}
-          />
-        </div> */}
-        <div className="flex flex-col">
+
+        <div className="flex flex-col flex-1">
           <label htmlFor="input-number-gas" className="text-[#404040] dark:text-[#A4A5AA] mb-1">
             Reward Amount
           </label>
@@ -268,11 +182,11 @@ export default function CreateOrderCardAdvanced({
       </div>
 
       {/* Gas Cost Settings */}
-      <div className="flex flex-col mt-4 lg:flex-row gap-6">
-        <div className="flex flex-col lg:grow">
+      <div className="flex flex-col mt-4 old-lg:flex-row gap-6">
+        <div className="flex flex-col flex-1">
           <label className="text-[#404040] dark:text-[#A4A5AA] mb-1">Gas Cost Token</label>
           <TokenSearchSelect
-            className="mt-2"
+            className=""
             searchSelectValue={inputValues.gasCostValueToken}
             changeHandler={(value) =>
               setInputValues({
@@ -282,25 +196,8 @@ export default function CreateOrderCardAdvanced({
             }
           />
         </div>
-        {/* <div className="flex flex-col lg:grow">
-          <Text>Gas Price</Text>
-          <NumberInput
-            className="mt-2"
-            value={inputValues.gasCostValueGasPrice.toString()}
-            onChange={(e) => {
-              const gasCostValueGasPrice = Number(e.target.value)
-              setInputValues({
-                ...inputValues,
-                gasCostValueGasPrice,
-              })
-            }}
-            min={0}
-            error={!!validationErrors?.gasCostValueGasPrice}
-            errorMessage={validationErrors?.gasCostValueGasPrice}
-            spellCheck={false}
-          />
-        </div> */}
-        <div className="flex flex-col">
+
+        <div className="flex flex-col flex-1">
           <label htmlFor="input-number-gas" className="text-[#404040] dark:text-[#A4A5AA] mb-1">
             Gas Price
           </label>
@@ -325,11 +222,11 @@ export default function CreateOrderCardAdvanced({
       </div>
 
       {/* Guarantee Settings */}
-      <div className="flex flex-col mt-4 lg:flex-row gap-6">
-        <div className="flex flex-col lg:grow">
+      <div className="flex flex-col mt-4 old-lg:flex-row gap-6">
+        <div className="flex flex-col flex-1">
           <label className="text-[#404040] dark:text-[#A4A5AA] mb-1">Guarantee Token</label>
           <TokenSearchSelect
-            className="mt-2"
+            className=""
             searchSelectValue={inputValues.guaranteeValueToken}
             changeHandler={(value) =>
               setInputValues({
@@ -339,19 +236,8 @@ export default function CreateOrderCardAdvanced({
             }
           />
         </div>
-        {/* <div className="flex flex-col lg:grow">
-          <Text>Guarantee GasPrice</Text>
-          <NumberInput
-            className="mt-2"
-            value={inputValues.guaranteeValueGasPrice.toString()}
-            onChange={(e) => setInputValues({ ...inputValues, guaranteeValueGasPrice: Number(e.target.value) })}
-            min={0}
-            error={!!validationErrors?.guaranteeValueGasPrice}
-            errorMessage={validationErrors?.guaranteeValueGasPrice}
-            spellCheck={false}
-          />
-        </div> */}
-        <div className="flex flex-col">
+
+        <div className="flex flex-col flex-1">
           <label htmlFor="input-number-gas" className="text-[#404040] dark:text-[#A4A5AA] mb-1">
             Guarantee GasPrice
           </label>
@@ -370,20 +256,8 @@ export default function CreateOrderCardAdvanced({
       </div>
 
       {/* Advanced Settings */}
-      <div className="flex flex-col lg:flex-row gap-6 mt-4">
-        {/* <div className="flex flex-col">
-          <Text>Execution window</Text>
-          <NumberInput
-            className="mt-2"
-            value={inputValues.executionWindow.toString()}
-            onChange={(e) => setInputValues({ ...inputValues, executionWindow: Number(e.target.value) })}
-            min={0}
-            error={!!validationErrors?.executionWindow}
-            errorMessage={validationErrors?.executionWindow}
-            spellCheck={false}
-          />
-        </div> */}
-        <div className="flex flex-col">
+      <div className="flex flex-col old-lg:flex-row gap-6 mt-4">
+        <div className="flex flex-col flex-1">
           <label htmlFor="input-number-gas" className="text-[#404040] dark:text-[#A4A5AA] mb-1">
             Execution window
           </label>
@@ -399,22 +273,8 @@ export default function CreateOrderCardAdvanced({
             className="h-12 p-3 rounded-6 border-normal dark:border-whiteDark hover:border-primary focus:border-primary dark:placeholder-white/60"
           />
         </div>
-        {/* <div className="flex flex-col ">
-          <Text>Reward transfer</Text>
-          <NumberInput
-            className="mt-2"
-            value={inputValues.rewardTransfer.toString()}
-            onChange={(e) => {
-              setInputValues({ ...inputValues, rewardTransfer: Number(e.target.value) })
-              setWasRewardTransferChanged(true)
-            }}
-            min={0}
-            error={!!validationErrors?.rewardTransfer}
-            errorMessage={validationErrors?.rewardTransfer}
-            spellCheck={false}
-          />
-        </div> */}
-        <div className="flex flex-col">
+
+        <div className="flex flex-col flex-1">
           <label htmlFor="input-number-gas" className="text-[#404040] dark:text-[#A4A5AA] mb-1">
             Reward transfer
           </label>
@@ -433,22 +293,8 @@ export default function CreateOrderCardAdvanced({
             className="h-12 p-3 rounded-6 border-normal dark:border-whiteDark hover:border-primary focus:border-primary dark:placeholder-white/60"
           />
         </div>
-        {/* <div className="flex flex-col ">
-          <Text>Gas cost transfer</Text>
-          <NumberInput
-            className="mt-2"
-            value={inputValues.gasCostTransfer.toString()}
-            onChange={(e) => {
-              setInputValues({ ...inputValues, gasCostTransfer: Number(e.target.value) })
-              setWasGasCostTransferChanged(true)
-            }}
-            min={0}
-            error={!!validationErrors?.gasCostTransfer}
-            errorMessage={validationErrors?.gasCostTransfer}
-            spellCheck={false}
-          />
-        </div> */}
-        <div className="flex flex-col">
+
+        <div className="flex flex-col flex-1">
           <label htmlFor="input-number-gas" className="text-[#404040] dark:text-[#A4A5AA] mb-1">
             Gas cost transfer
           </label>
@@ -468,7 +314,7 @@ export default function CreateOrderCardAdvanced({
           />
         </div>
       </div>
-      <div className="flex flex-col gap-4 md:gap-0 md:items-end md:flex-row md:justify-between mt-4">
+      <div className="flex flex-col old-sm:flex-row old-sm:items-end old-sm:justify-between gap-4 mt-4 old-sm:mt-8">
         <Receipt
           //TODO pass correct token names
           gasAmount={inputValues.gasAmount}
@@ -479,7 +325,7 @@ export default function CreateOrderCardAdvanced({
         />
         <Buttons
           onClick={handleSubmit}
-          className="grow md:grow-0 bg-primary hover:bg-primary-hbr border-solid border-1 border-primary text-white dark:text-white/[.87] text-[14px] font-semibold leading-[22px] inline-flex items-center justify-center rounded-[4px] px-[20px] h-[44px]"
+          className="bg-primary hover:bg-primary-hbr border-solid border-1 border-primary text-white dark:text-white/[.87] text-[14px] font-semibold leading-[22px] inline-flex items-center justify-center rounded-[4px] px-[20px] h-[44px]"
         >
           {"Create Gas Order"}
         </Buttons>
