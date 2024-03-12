e83769202dcace76001543847443f7a116a04c10
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-03-12 22:40:39 +0200

    add our favicon

--- packages/frontend/src/pages/_document.tsx
+++ packages/frontend/src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" dir="ltr">
       <Head>
-        <link rel="icon" href="/hexadash-nextjs/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
