a96a14f508dc3e89b7f24089099b3f1134815b87
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-03-14 15:09:33 +0200

    add transaction result modal to tx create page

--- packages/frontend/src/pages/admin/tx/create/index.tsx
+++ packages/frontend/src/pages/admin/tx/create/index.tsx
@@ -9,6 +9,8 @@ import { Buttons } from "@/components/buttons"
 import dayjs from "dayjs"
 import CreateTxCard from "@/components/CreateTxCards/CreateTxCard"
 import { useState } from "react"
+import DialogWindow from "@/components/DialogWindow"
+import { TailSpin } from "react-loader-spinner"
 
 const items: TabsProps["items"] = [
   {
@@ -44,6 +46,54 @@ const CreateTx = () => {
         title="Create Transaction"
         className="flex items-center justify-between px-8 xl:px-[15px] pt-[18px] pb-6 sm:pb-[30px] bg-transparent sm:flex-col"
       />
+
+      {showDialogWindow ? (
+        Boolean(transactionDetails) ? (
+          <DialogWindow
+            onClose={() => {
+              setShowDialogWindow(false)
+              setTransactionDetails(null)
+            }}
+            isClosable={true}
+            title="Transaction Result"
+            description={
+              transactionDetails.error ? (
+                "There seems to be an error :("
+              ) : (
+                <div className="flex flex-col break-words gap-4">
+                  From
+                  <span>{transactionDetails.from}</span>
+                  To
+                  <span>{transactionDetails.to}</span>
+                  Transaction Hash
+                  <span>{transactionDetails.transactionHash}</span>
+                  Status
+                  <span>{transactionDetails.status}</span>
+                </div>
+              )
+            }
+          ></DialogWindow>
+        ) : (
+          <DialogWindow
+            isClosable={false}
+            title="Creating Transaction"
+            description={
+              <div className="flex justify-center">
+                <TailSpin
+                  height={40}
+                  width={40}
+                  color={"#009688"}
+                  ariaLabel="tail-spin-loading"
+                  radius="0"
+                  wrapperStyle={{}}
+                  wrapperClass=""
+                  visible={true}
+                />
+              </div>
+            }
+          ></DialogWindow>
+        )
+      ) : null}
       <div className="min-h-[715px] lg:min-h-[580px] flex-1 h-auto px-8 xl:px-[15px] pb-[30px] bg-transparent">
         <div className="h-full w-full">
           <Cards headless className="max-w-[1024px] mx-auto">
