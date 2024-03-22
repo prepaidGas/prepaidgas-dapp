7d86b87b6df0e81e93171422977515c008d32fed
web3skeptic <44979695+web3skeptic@users.noreply.github.com>
2024-03-22 20:08:42 +0100

    chore: update license

--- packages/contracts/contracts/tools/Validators.sol
+++ packages/contracts/contracts/tools/Validators.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: GPL-3.0-only
 // @notice !!!IMPORTANT this contract is not gonna be utilized for the first version of the protocol
 pragma solidity 0.8.20;
 
