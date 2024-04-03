8a90d20b145a8905277b0c1789a7aac8cb191b34
SteMak <chestedos@gmail.com>
2024-04-04 01:31:10 +0200

    silence scripts and tests

--- packages/contracts/scripts/constants/index.js
+++
@@ -1,9 +0,0 @@
-const executorConstatns = require("./executor.js")
-const gasOrderConstatns = require("./gasOrder.js")
-const testingConstatns = require("./testing.js")
-
-module.exports = {
-  ...executorConstatns,
-  ...gasOrderConstatns,
-  ...testingConstatns,
-}
