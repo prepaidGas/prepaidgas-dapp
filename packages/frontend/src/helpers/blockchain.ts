72fb43ad7ebc0540241d7121e569773a66c63ab2
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-04-07 22:41:43 +0300

    update treasury and prepaidgas helpers

--- packages/frontend/src/helpers/blockchain.ts
+++ packages/frontend/src/helpers/blockchain.ts
@@ -1,14 +1,23 @@
-import { LOCAL_EVM_PREPAIDGAS_SMART_CONTRACT } from "@/constants" // @todo improve path to the constants
-
+import { LOCAL_EVM_PREPAIDGAS_SMART_CONTRACT, LOCAL_EVM_TREASURY_SMART_CONTRACT } from "@/constants" // @todo improve path to the constants
 
 const prepaidGasCoreContractAddress = () => {
-    if(process.env.NETWORK == "production") {
-        return "0x"; // @todo finalize
-    } else if(process.env.NETWORK == "testnet") {
-        return "0x"; // @todo finalize
-    } else {
-        return LOCAL_EVM_PREPAIDGAS_SMART_CONTRACT;
-    } 
+  if (process.env.NETWORK == "production") {
+    return "0x" // @todo finalize
+  } else if (process.env.NETWORK == "testnet") {
+    return "0x" // @todo finalize
+  } else {
+    return LOCAL_EVM_PREPAIDGAS_SMART_CONTRACT
+  }
 }
 
-export { prepaidGasCoreContractAddress };
+const prepaidGasTreasuryContractAddress = () => {
+  if (process.env.NETWORK == "production") {
+    return "0x" // @todo finalize
+  } else if (process.env.NETWORK == "testnet") {
+    return "0x" // @todo finalize
+  } else {
+    return LOCAL_EVM_TREASURY_SMART_CONTRACT
+  }
+}
+
+export { prepaidGasCoreContractAddress, prepaidGasTreasuryContractAddress }
