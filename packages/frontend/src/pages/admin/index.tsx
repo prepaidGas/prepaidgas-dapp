9f9c2dc9d32e28766cac3e91abbecbd7e9a9988f
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-05-17 15:19:10 +0300

    make "/" and "/admin" path return create order page

--- packages/frontend/src/pages/admin/index.tsx
+++ packages/frontend/src/pages/admin/index.tsx
@@ -1,94 +1,25 @@
-import dynamic from "next/dynamic"
-import { Row, Col, Skeleton } from "antd"
-import { PageHeaders } from "@/components/page-headers"
+"use client"
+import { useEffect } from "react"
+import CreateOrder from "./order/create"
+import Router from "next/router"
 
-const OverviewDataList = dynamic(() => import("@/dashboard/demo-1/OverviewDataList"), {
-  loading: () => (
-    <>
-      <Skeleton active />
-    </>
-  ),
-})
-const SalesReport = dynamic(() => import("@/dashboard/demo-1/SalesReport"), {
-  loading: () => (
-    <>
-      <Skeleton active />
-    </>
-  ),
-})
-const SalesGrowth = dynamic(() => import("@/dashboard/demo-1/SalesGrowth"), {
-  loading: () => (
-    <>
-      <Skeleton active />
-    </>
-  ),
-})
-const SalesByLocation = dynamic(() => import("@/dashboard/demo-1/SalesByLocation"), {
-  loading: () => (
-    <>
-      <Skeleton active />
-    </>
-  ),
-})
-const TopSellingProduct = dynamic(() => import("@/dashboard/demo-1/TopSellingProducts"), {
-  loading: () => (
-    <>
-      <Skeleton active />
-    </>
-  ),
-})
-const BrowserState = dynamic(() => import("@/dashboard/demo-1/BrowserState"), {
-  loading: () => (
-    <>
-      <Skeleton active />
-    </>
-  ),
-})
+// Function to check if the user is logged in
+const isUserLoggedIn = () => {
+  const isLoggedIn = false
 
-const DemoOne = () => {
-  const PageRoutes = [
-    {
-      path: "admin",
-      breadcrumbName: "Dashboard",
-    },
-    {
-      path: "first",
-      breadcrumbName: "Demo 1",
-    },
-  ]
-  return (
-    <>
-      <PageHeaders
-        routes={PageRoutes}
-        title="Dashboard"
-        className="flex items-center justify-between px-8 xl:px-[15px] pt-[18px] pb-6 sm:pb-[30px] bg-transparent sm:flex-col"
-      />
-      <div className="min-h-[715px] lg:min-h-[580px] flex-1 h-auto px-8 xl:px-[15px] pb-[30px] bg-transparent">
-        <Row gutter={25}>
-          <Col xxl={12} xs={24}>
-            <OverviewDataList />
-          </Col>
-          <Col xxl={12} xs={24} className="mb-[25px]">
-            <SalesReport />
-          </Col>
-          <Col xxl={8} xs={24} className="mb-[25px]">
-            <SalesGrowth />
-          </Col>
-          <Col xxl={16} xs={24} className="mb-[25px]">
-            <SalesByLocation />
-          </Col>
-        </Row>
-        <Row gutter={25}>
-          <Col xl={12} xs={24} className="mb-[25px]">
-            <TopSellingProduct />
-          </Col>
-          <Col xl={12} xs={24} className="mb-[25px]">
-            <BrowserState />
-          </Col>
-        </Row>
-      </div>
-    </>
-  )
+  return isLoggedIn
 }
 
-export default DemoOne
+const Admin = () => {
+  const isLoggedIn = isUserLoggedIn()
+
+  //todo: remove old login logic
+  // return isLoggedIn ? <DemoOne /> : <SignIn />
+  // useEffect(() => {
+  //   Router.push("/admin/order/create")
+  // }, [])
+
+  return <CreateOrder />
+}
+
+export default Admin
