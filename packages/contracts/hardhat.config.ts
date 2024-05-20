1ba656719b44b0b3c937914ef0695a3ca5675016
SteMak <chestedos@gmail.com>
2024-05-20 12:57:21 +0200

    contract test deploy helpers

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -3,6 +3,7 @@ import "@nomicfoundation/hardhat-ethers"
 import "@nomicfoundation/hardhat-toolbox"
 import "solidity-coverage"
 import "hardhat-contract-sizer"
+import "dotenv/config"
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -17,6 +18,13 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhat: {},
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY ?? ""}`,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY ?? ""],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY ?? "",
   },
 }
 
