f3dc3ce6071a77fb63889186ad18a092f0e696d9
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-05-22 16:13:02 +0300

    updated hexadash wrapper for antd bttns

--- packages/frontend/src/components/buttons/index.tsx
+++ packages/frontend/src/components/buttons/index.tsx
@@ -1,54 +1,11 @@
-import { useState } from "react"
-import { Button } from "antd"
+import { ReactNode, useState } from "react"
+import { Button, ButtonProps } from "antd"
 
-function Buttons(props: any) {
-  const {
-    type,
-    shape,
-    icon,
-    size,
-    outlined,
-    ghost,
-    transparented,
-    raised,
-    squared,
-    color,
-    social,
-    load,
-    className,
-    children,
-    ...rest
-  } = props
-  const [state, setState] = useState({
-    loading: false,
-  })
+type PropsWithChildren<P> = P & { children?: ReactNode }
 
-  const enterLoading = () => {
-    setState({ loading: true })
-  }
-
-  return (
-    <Button
-      squared={squared}
-      outlined={outlined ? 1 : 0}
-      ghost={ghost}
-      transparent={transparented ? 1 : 0}
-      raised={raised ? 1 : 0}
-      data={type}
-      size={size}
-      shape={shape}
-      type={type}
-      icon={icon}
-      color={color}
-      social={social}
-      onClick={load && enterLoading}
-      loading={state.loading}
-      className={className}
-      {...rest}
-    >
-      {children}
-    </Button>
-  )
+function Buttons(props: PropsWithChildren<ButtonProps>) {
+  const { children, ...rest } = props
+  return <Button {...rest}>{children}</Button>
 }
 
 function BtnGroup({ children }: any) {
