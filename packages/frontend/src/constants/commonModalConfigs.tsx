6eb8b648032827ddc085bbbee6423ba70e41e52b
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-05-23 20:34:43 +0300

    replace Modal instance geters with a compilation of common modal configs

---
+++ packages/frontend/src/constants/commonModalConfigs.tsx
@@ -0,0 +1,40 @@
+import CustomConnectBttn from "@/components/CustomConnectBttn"
+import { HookAPI } from "antd/lib/modal/useModal"
+import { TailSpin } from "react-loader-spinner"
+import { ModalFuncProps } from "antd"
+
+const WalletConnectionConfig: ModalFuncProps = {
+  title: "Wallet Connection",
+  // icon: <UilWallet />,
+  content: "Please connect your wallet to continue with order creation",
+  footer: (_, { OkBtn, CancelBtn }) => <>{/* <CustomConnectBttn onClick={() => instance.destroy()} /> */}</>,
+}
+
+const SuccessConfig: ModalFuncProps = {
+  title: "Success",
+  closable: true,
+}
+
+const ErrorConfig: ModalFuncProps = { title: "Error", closable: true, content: "" }
+
+const ProcessingConfig: ModalFuncProps = {
+  title: "Processing",
+  // icon: <UiProcess />,
+  closable: false,
+  content: (
+    <div className="flex justify-center">
+      <TailSpin
+        height={40}
+        width={40}
+        color={"#009688"}
+        ariaLabel="tail-spin-loading"
+        radius="0"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  ),
+}
+
+export default { WalletConnectionConfig, ProcessingConfig, ErrorConfig, SuccessConfig }
